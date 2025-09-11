60ab53de36e8cc02bebaab1e2ea88dedaf7a6f18
anaelenafdez <anaelenafdez@gmail.com>
2025-09-11 17:57:49 +0200

    Commit con mi primer reto

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BitAnima.py
@@ -0,0 +1,51 @@
+"""ENUNCIADO
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */"""
+
+#https://www.python.org/
+# Comentario en una línea
+"""
+Esto es un comentario en varias líneas
+"""
+
+'''
+Esto es un comentario en varias líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor"
+
+#Cuando se escribe una variable en mayúscula, se considera una constante por convención, pero no lo es realmente.
+
+print("Hola, Python!")
+
+my_int = 1
+my_float = 0.5
+my_bool = True
+my_str = "Cadena de texto"
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_str))
